d7915684f64794debb3ca5345074f429dcbaa886
Артем Саранча <98779874+Aprioren@users.noreply.github.com>
2023-03-01 12:35:40 +0200

    figing a search img

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => (
   >
     <div className='absolute w[50%] inset-0 gradient-01'/>
     <div className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}>
-      <img src="/search.svg" alt="search"  className='w-[24px] h-[24px] object-contain'/>
+      <img src="./search.svg" alt="search"  className='w-[24px] h-[24px] object-contain'/>
       <h2 className='font-extrabold text-[24px] leading-[30px] text-white'>
         Sarancha
       </h2>
